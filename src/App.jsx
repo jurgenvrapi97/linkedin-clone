26b4e517f0db7bdb9efaeb40e8b97e4d13b919ce
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-02-19 16:17:26 +0100

    merge tra main e aside

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyAside from "./components/MyAside";
 import Mainprofile from "./components/Mainprofile";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <Mainprofile />
-      <MyAside />
+      <Container>
+        <Row>
+          <Col md={8} lg={8}>
+            <Mainprofile />
+          </Col>
+          <Col>
+            <MyAside />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- src/components/Mainprofile.jsx
+++ src/components/Mainprofile.jsx
@@ -5,58 +5,52 @@ import { Container, Row, Col } from "react-bootstrap";
 const Mainprofile = () => {
   return (
     <>
-      <Container fluid className="mt-0">
-        <Row>
-          <Col xs={12} md={9}>
-            <Card>
-              <div className=" position-relative container-fluid">
-                <Card.Img variant="top" src="https://placebear.com/900/400" />
+      <Card>
+        <div className=" position-relative container-fluid">
+          <Card.Img variant="top" src="https://placebear.com/900/400" />
 
+          <img
+            src="https://placekitten.com/200"
+            className="rounded-circle position-absolute bottom-0 start-0 img-fluid mw-100"
+          />
+        </div>
+
+        <Card.Body className="pt-5 ">
+          <Row>
+            <Col xs={6}>
+              <Card.Title>-Inserire nome profilo-</Card.Title>
+              <Card.Text className="pt-0">-inserisci lavoro-</Card.Text>
+              <p>
+                inserisci info di contatto -
+                <a src="#">Informazioni di contatto</a>
+              </p>
+              <p className="text-secondary">
+                -inserisci numero collegamenti - collegamenti
+              </p>
+              <Button
+                variant="light"
+                className="border border-black rounded-pill py-1"
+              >
+                Altro
+              </Button>
+            </Col>
+            <Col>
+              <p>
                 <img
-                  src="https://placekitten.com/200"
-                  className="rounded-circle position-absolute bottom-0 start-0 img-fluid mw-100"
+                  src="https://media.licdn.com/dms/image/C4E0BAQHYgix-Ynux1A/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1716422400&v=beta&t=5MUJe7JW7qN_AhLIvXWy09nSa-yX3GS-ThImsm3_xqE"
+                  alt="logo"
+                  width={40}
                 />
-              </div>
+                <span>EPICODE</span>
+              </p>
+              <p>
+                <img src=""></img>
+              </p>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
 
-              <Card.Body className="pt-5 ">
-                <Row>
-                  <Col xs={6}>
-                    <Card.Title>-Inserire nome profilo-</Card.Title>
-                    <Card.Text className="pt-0">-inserisci lavoro-</Card.Text>
-                    <p>
-                      inserisci info di contatto -
-                      <a src="#">Informazioni di contatto</a>
-                    </p>
-                    <p className="text-secondary">
-                      {" "}
-                      -inserisci numero collegamenti - collegamenti
-                    </p>
-                    <Button
-                      variant="light"
-                      className="border border-black rounded-pill py-1"
-                    >
-                      Altro
-                    </Button>
-                  </Col>
-                  <Col>
-                    <p>
-                      <img
-                        src="https://media.licdn.com/dms/image/C4E0BAQHYgix-Ynux1A/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1716422400&v=beta&t=5MUJe7JW7qN_AhLIvXWy09nSa-yX3GS-ThImsm3_xqE"
-                        alt="logo"
-                        width={40}
-                      />
-                      <span>EPICODE</span>
-                    </p>
-                    <p>
-                      <img src=""></img>
-                    </p>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
       <Container fluid className="pt-3">
         <Row>
           <Col xs={12} md={9}>
