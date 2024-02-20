34f1cb4114a2a2b002054e067bd043d2de1e142c
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-02-20 16:50:02 +0100

    BrowserRouter in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,50 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/assets/main.css";
 import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-// import MyAside from "./components/MyAside";
-// import Mainprofile from "./components/Mainprofile";
-// import { Col, Container, Row } from "react-bootstrap";
+import MyAside from "./components/MyAside";
+import Mainprofile from "./components/Mainprofile";
+import { Col, Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import MyNav from "./components/MyNav";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import MSGbar from "./components/MSGbar";
-// import Experiences from "./components/Experiences";
+import Experiences from "./components/Experiences";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="bg-background">
-      {/* <MyNav />
-      <Container>
-        <Row>
-          <Col md={8} lg={8}>
-            <Mainprofile />
-            <Experiences />
-          </Col>
-          <Col>
-            <MyAside />
-          </Col>
-        </Row>
-        <MSGbar />
-        <Footer />
-      </Container> */}
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="bg-background">
         <MyNav />
-        <MSGbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+
+        <Container>
+          <Routes>
+            <Route
+              path="/profile"
+              element={
+                <>
+                  <Row>
+                    <Col md={8} lg={8}>
+                      <Mainprofile />
+                      <Experiences />
+                    </Col>
+                    <Col>
+                      <MyAside />
+                    </Col>
+                  </Row>
+                  <MSGbar />
+                  <Footer />
+                </>
+              }
+            />
+
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+      </div>
+    </BrowserRouter>
   );
 }
 
