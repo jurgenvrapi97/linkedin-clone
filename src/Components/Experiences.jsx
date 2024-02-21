5bebd10100daea0f337f377cc6f2e4a76b0a6e9a
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-02-21 12:36:27 +0100

    modifica

--- src/Components/Experiences.jsx
+++ src/Components/Experiences.jsx
@@ -1,33 +1,88 @@
 import Card from "react-bootstrap/Card";
 import { CardBody, ListGroup } from "react-bootstrap";
+import AddExp from "./AddExp";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchExperiences } from "../redux/action";
+import { fetchExperiencesAction } from "../redux/action";
+import ModaleModificaExperience from "./ModaleModificaExperience";
 
 const Experiences = () => {
+  const formatDate = (dateISO) => {
+    let dateObj = new Date(dateISO);
+    let year = dateObj.getFullYear();
+    let month = dateObj.getMonth();
+    let date = dateObj.getDate();
+    let dateStr = [date, month + 1, year].join("/");
+    return dateStr;
+  };
+
+  const dispatch = useDispatch();
+  const tokens = useSelector((state) => state.user.tokens);
+  const profile = useSelector((state) => state.user.user);
+  const experiences = useSelector((state) => state.experiences.allExperiences);
+
+  useEffect(() => {
+    if (profile._id) {
+      dispatch(fetchExperiences(tokens.jurgen, profile._id));
+      console.log("partita");
+    }
+  }, [dispatch, profile._id]);
+
+  const handleDelete = (chiave) => {
+    dispatch(
+      fetchExperiencesAction(tokens.jurgen, profile._id, chiave, "DELETE")
+    );
+  };
+
+  const handleEdit = (chiave) => {
+    dispatch(fetchExperiencesAction(tokens.jurgen, profile._id, chiave, "GET"));
+  };
+  const handleChanges = (chiave) => {
+    dispatch(fetchExperiencesAction(tokens.jurgen, profile._id, chiave, "PUT"));
+  };
+
   return (
-    <Card className="d-none d-md-flex" md={12} lg={12}>
-      <Card.Title className="p-3 ">Esperienze</Card.Title>
+    <>
+      <Card className="d-none d-md-flex" md={12} lg={12}>
+        <Card.Title className="p-3 d-flex justify-content-between ">
+          Esperienze <AddExp />
+        </Card.Title>
+        {experiences && experiences.length ? (
+          experiences.map((exp) => (
+            <div key={exp._id}>
+              <CardBody className="p-1">
+                <ListGroup>
+                  <ListGroup.Item className="border border-0 d-flex">
+                    <div>
+                      <img src="https://placekitten.com/200"></img>
+                    </div>
 
-      <CardBody className="p-1">
-        <ListGroup>
-          <ListGroup.Item className="border border-0 d-flex">
-            <div>
-              <img
-                src="https://media.licdn.com/dms/image/C4E0BAQHYgix-Ynux1A/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1716422400&v=beta&t=5MUJe7JW7qN_AhLIvXWy09nSa-yX3GS-ThImsm3_xqE"
-                width="50px"
-                height="50px"
-              ></img>
-            </div>
+                    <div className=" ms-3 ">
+                      <h6 className="mb-0 lh-sm">{exp.username}</h6>
+                      <p>
+                        {exp.role} · {exp.company}
+                      </p>
+                      <p className="mb-0 fs-6 lh-sm text-secondary">
+                        {formatDate(exp.startDate)} · {formatDate(exp.endDate)}
+                      </p>
+                      <p className="mb-0 fs-6 lh-sm text-secondary">
+                        {exp.area}
+                      </p>
+                    </div>
+                  </ListGroup.Item>
+                </ListGroup>
+              </CardBody>
 
-            <div className=" ms-3 ">
-              <h6 className="mb-0 lh-sm">-inserisci lavoro-</h6>
-              <p className="mb-0 fs-6 lh-sm text-secondary">
-                inserisci data · inizio lavoro da
-              </p>
-              <p className="mb-0 fs-6 lh-sm text-secondary">inserisci città</p>
+              <button onClick={() => handleDelete(exp._id)}>cancella</button>
+              <ModaleModificaExperience chiave={exp._id} />
             </div>
-          </ListGroup.Item>
-        </ListGroup>
-      </CardBody>
-    </Card>
+          ))
+        ) : (
+          <p>Nessuna esperienza trovata per questo profilo.</p>
+        )}
+      </Card>
+    </>
   );
 };
 
---
+++ src/Components/ModaleModificaExperience.jsx
@@ -0,0 +1,169 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExperiencesCreate } from "../redux/action";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Col, Container, Row } from "react-bootstrap";
+import { fetchExperiencesAction } from "../redux/action";
+
+const ModaleModificaExperience = ({ chiave }) => {
+  const dispatch = useDispatch();
+  const username = useSelector((state) => {
+    return state.user.user;
+  });
+  const tokens = useSelector((state) => {
+    return state.user.tokens;
+  });
+
+  const modificaQuestoCommento = useSelector((state) => state.action.data);
+  console.log(modificaQuestoCommento);
+
+  const sicuroDiVolerModificare = () => {
+    console.log(modificaQuestoCommento);
+    setForm({
+      role: modificaQuestoCommento.role,
+      company: modificaQuestoCommento.company,
+      startDate: modificaQuestoCommento.startDate,
+      endDate: modificaQuestoCommento.endDate,
+      description: modificaQuestoCommento.description,
+      area: modificaQuestoCommento.area,
+    });
+  };
+
+  const [form, setForm] = useState({
+    role: "",
+    company: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    area: "",
+  });
+
+  //state modale
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(true),
+      dispatch(
+        fetchExperiencesAction(tokens.jurgen, username._id, chiave, "GET")
+      );
+
+    if (modificaQuestoCommento) {
+      console.log("sono dentro");
+    } else {
+      console.log("modificaQuestoCommento è null");
+    }
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShow(false);
+    dispatch(fetchExperiencesCreate(tokens.jurgen, username._id, form));
+  };
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        <i className="bi bi-plus-circle-fill"></i>
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header className="bg-background" closeButton>
+          <Button onClick={() => sicuroDiVolerModificare}>
+            Sei sicuro di voler modificare?
+          </Button>
+          <Modal.Title>Inserisci una nuova esperienza</Modal.Title>
+        </Modal.Header>
+        <Container className="py-2">
+          <form onSubmit={handleSubmit}>
+            <Row className="mb-2">
+              <Col className="d-flex flex-column">
+                <label htmlFor="role">Ruolo</label>
+                <input
+                  type="text"
+                  id="role"
+                  name="role"
+                  value={form.role}
+                  onChange={handleChange}
+                />
+              </Col>
+              <Col className="d-flex flex-column">
+                <label htmlFor="company">Compagnia</label>
+                <input
+                  type="text"
+                  id="company"
+                  name="company"
+                  value={form.company}
+                  onChange={handleChange}
+                />
+              </Col>
+            </Row>
+            <Row className="mb-2 justify-content-center">
+              <Col className="d-flex flex-column">
+                <label htmlFor="startDate">Data di inizio</label>
+                <input
+                  type="date"
+                  id="startDate"
+                  name="startDate"
+                  value={form.startDate}
+                  onChange={handleChange}
+                />
+              </Col>
+              <Col className="d-flex flex-column">
+                <label className="d-block" htmlFor="endDate">
+                  Data di fine
+                </label>
+                <input
+                  type="date"
+                  id="endDate"
+                  name="endDate"
+                  value={form.endDate}
+                  onChange={handleChange}
+                />
+              </Col>
+            </Row>
+            <Row className="aligne-item-center mb-3">
+              <Col className="d-flex flex-column">
+                <label className="d-block" htmlFor="area">
+                  Area
+                </label>
+                <input
+                  type="text"
+                  id="area"
+                  name="area"
+                  value={form.area}
+                  onChange={handleChange}
+                />
+              </Col>
+              <Col className="d-flex col-12 flex-column">
+                <label htmlFor="description">Descrizione</label>
+                <textarea
+                  id="description"
+                  name="description"
+                  value={form.description}
+                  onChange={handleChange}
+                />
+              </Col>
+            </Row>
+            <Row className="justify-content-end">
+              <Col className="col-3 d-flex justify-content-end">
+                <Button type="submit">AGGIUNGI</Button>
+              </Col>
+            </Row>
+          </form>
+        </Container>
+      </Modal>
+    </>
+  );
+};
+
+export default ModaleModificaExperience;
