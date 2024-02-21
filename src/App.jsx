30ec2cb72ce4fabac02958fd76cba094e676b38a
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-02-21 12:52:26 +0100

    img

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../src/assets/main.css";
-import "./App.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import MyAside from "./components/MyAside";
-import Mainprofile from "./components/Mainprofile";
-import { Col, Container, Row } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.min.css";
-import MyNav from "./components/MyNav";
-import Footer from "./components/Footer";
-import MSGbar from "./components/MSGbar";
-import Experiences from "./components/Experiences";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/assets/main.css'
+import './App.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import MyAside from './components/MyAside'
+import Mainprofile from './components/Mainprofile'
+import { Col, Container, Row } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import MyNav from './components/MyNav'
+import Footer from './components/Footer'
+import MSGbar from './components/MSGbar'
+import Experiences from './components/Experiences'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import AddExp from './components/AddExp'
+import Test from './components/Test'
+import ImageUpload from './components/ImageUpload'
 
 function App() {
   const userSelector = (state) => state.user.user
@@ -30,7 +33,28 @@ function App() {
                 <>
                   <Row>
                     <Col md={8} lg={8}>
-                      <Mainprofile selector={userSelector}/>
+                      <Mainprofile selector={userSelector} />
+                      <Experiences />
+                      <AddExp />
+                      <ImageUpload />
+                      <Test />
+                    </Col>
+                    <Col>
+                      <MyAside />
+                    </Col>
+                  </Row>
+                  <MSGbar />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/profile/:userId"
+              element={
+                <>
+                  <Row>
+                    <Col md={8} lg={8}>
+                      <Mainprofile selector={idSelector} />
                       <Experiences />
                     </Col>
                     <Col>
@@ -42,26 +66,12 @@ function App() {
                 </>
               }
             />
-            <Route path="/profile/:userId" element={
-              <>
-                <Row>
-                  <Col md={8} lg={8}>
-                    <Mainprofile selector={idSelector}/>
-                    <Experiences />
-                  </Col>
-                  <Col>
-                    <MyAside />
-                  </Col>
-                </Row>
-                <MSGbar />
-                <Footer />
-                </>} />
             <Route path="/" element={<Home />} />
           </Routes>
         </Container>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/ImageUpload.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+function ImageUpload() {
+  const [file, setFile] = useState()
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0])
+  }
+
+  const handleUpload = async () => {
+    let formData = new FormData()
+
+    formData.append('profile', file)
+
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/profile/65d3150324f605001937d469/picture',
+        {
+          method: 'POST',
+          body: formData,
+          headers: {
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQzMTUwMzI0ZjYwNTAwMTkzN2Q0NjkiLCJpYXQiOjE3MDgzMzIyOTEsImV4cCI6MTcwOTU0MTg5MX0.Dvp9xjhvg1QFWbOGGaWpXWP1M-7JHhQLM0zCwLO1doM',
+          },
+        }
+      )
+      let data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Carica</button>
+    </div>
+  )
+}
+
+export default ImageUpload
