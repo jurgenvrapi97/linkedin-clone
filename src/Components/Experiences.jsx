6cd2b0581ddddec51d796fa962436d0d3ea2fac5
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-02-22 17:37:09 +0100

    4.3

--- src/Components/Experiences.jsx
+++ src/Components/Experiences.jsx
@@ -1,46 +1,47 @@
-import Card from 'react-bootstrap/Card'
-import { Button, CardBody, ListGroup } from 'react-bootstrap'
-import AddExp from './AddExp'
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { fetchExperiences } from '../redux/action'
-import { fetchExperiencesAction } from '../redux/action'
-import ModaleModificaExperience from './ModaleModificaExperience'
+import Card from "react-bootstrap/Card";
+import { Button, CardBody, ListGroup } from "react-bootstrap";
+import AddExp from "./AddExp";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchExperiences } from "../redux/action";
+import { fetchExperiencesAction } from "../redux/action";
+import ModaleModificaExperience from "./ModaleModificaExperience";
 
 const Experiences = ({ tokenKey, selector }) => {
   const formatDate = (dateISO) => {
-    let dateObj = new Date(dateISO)
-    let year = dateObj.getFullYear()
-    let month = dateObj.getMonth()
-    let date = dateObj.getDate()
-    let dateStr = [date, month + 1, year].join('/')
-    return dateStr
-  }
+    let dateObj = new Date(dateISO);
+    let year = dateObj.getFullYear();
+    let month = dateObj.getMonth();
+    let date = dateObj.getDate();
+    let dateStr = [date, month + 1, year].join("/");
+    return dateStr;
+  };
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   // const tokens = useSelector((state) => state.user.tokens);
-  const profile = useSelector(selector)
-  const experiences = useSelector((state) => state.experiences.allExperiences)
-  const newExperience = useSelector((state) => state.create.experiences)
-  const user = useSelector((state) => state.user.user)
-  console.log(user)
-  console.log('il profile è:', profile)
+  const profile = useSelector(selector);
+  const experiences = useSelector((state) => state.experiences.allExperiences);
+  const newExperience = useSelector((state) => state.create.experiences);
+  const user = useSelector((state) => state.user.user);
+  // console.log(user)
+  // console.log('il profile è:', profile)
   // const nienteData = useSelector((state) => state.action.data);
   // console.log("niente", nienteData);
 
   useEffect(() => {
     if (profile._id) {
-      dispatch(fetchExperiences(tokenKey, profile._id))
-      console.log('partita')
+      dispatch(fetchExperiences(tokenKey, profile._id));
+      console.log("partita");
     }
-  }, [dispatch, profile._id, newExperience, experiences.length])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch, profile._id, newExperience, experiences.length]);
 
-  console.log(experiences.length)
+  // console.log(experiences.length)
 
   const handleDelete = (chiave) => {
-    dispatch(fetchExperiencesAction(tokenKey, profile._id, chiave, 'DELETE'))
-    dispatch(fetchExperiences(tokenKey, profile._id))
-  }
+    dispatch(fetchExperiencesAction(tokenKey, profile._id, chiave, "DELETE"));
+    dispatch(fetchExperiences(tokenKey, profile._id));
+  };
 
   return (
     <>
@@ -64,7 +65,7 @@ const Experiences = ({ tokenKey, selector }) => {
                           <h5 className="mb-0 lh-sm">{exp.role}</h5>
                           <p className="mt-1 mb-2">{exp.company}</p>
                           <p className="mb-0 text-secondary fw-light">
-                            da {formatDate(exp.startDate)} · a{' '}
+                            da {formatDate(exp.startDate)} · a{" "}
                             {formatDate(exp.endDate)}
                           </p>
                           <p className="mb-0 text-secondary fw-light">
@@ -96,7 +97,7 @@ const Experiences = ({ tokenKey, selector }) => {
         )}
       </Card>
     </>
-  )
-}
+  );
+};
 
-export default Experiences
+export default Experiences;
